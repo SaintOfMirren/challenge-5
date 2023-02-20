07d0adea9050f3fda83f3ce2965138ed888d4430
SaintOfMirren <drew.miller2015@gmail.com>
2023-02-20 13:31:01 -0800

    finalized code

--- Assets/script.js
+++ Assets/script.js
@@ -18,23 +18,84 @@ $(document).ready(function () {
   $('#currentDay').text(currentDay);
 
 
-  $('.entry').hover(function () {
-    $(this).prop('contenteditable', true);
-  }, function () {
-    $(this).prop('contenteditable', false);
+  // $('.entry').focus(function () {
+  //   $(this).prop('contenteditable', true);
+  // }, function () {
+  //   $(this).prop('contenteditable', false);
 
-    // Save the text to local storage
-    var text = $(this).text();
-    var hour = $(this).data('hour');
-    localStorage.setItem(hour, text);
-  });
+  //   // Save the text to local storage
+  //   var text = $(this).text();
+  //   var hour = $(this).data('entry');
+  //   localStorage.setItem(hour, text);
+});
 
-  // Load the text from local storage
-  $('.hour').each(function () {
-    var hour = $(this).data('hour');
-    var text = localStorage.getItem(hour);
-    if (text) {
-      $(this).text(text);
+// Load the text from local storage
+// $('.entry').each(function () {
+//   var hour = $(this).data('entry');
+//   var text = localStorage.getItem(hour);
+//   if (text) {
+//     $(this).text(text);
+//   }
+// });
+
+// Get all the div elements with the "editable" class
+const editableDivs = document.querySelectorAll('.entry');
+
+// Loop through each editable div and attach a click event listener
+editableDivs.forEach(div => {
+  // Add the placeholder text to the div
+  div.textContent = div.getAttribute('data-placeholder');
+
+  div.addEventListener('click', () => {
+    // if the div already has stored value remove it when clicked
+    const storedValue = localStorage.getItem(div.id);
+    if (storedValue) {
+      div.textContent = '';
     }
+    // If the div has the placeholder text, clear it
+    if (div.textContent === div.getAttribute('data-placeholder')) {
+      div.textContent = '';
+    }
+
+    // Check if an input element already exists in the div
+    let input = div.querySelector('input');
+
+    if (!input) {
+      // If an input element does not exist, create one and append it to the clicked div
+      input = document.createElement('input');
+      div.appendChild(input);
+    }
+
+    // Set the value of the input element to the current text in the div
+    input.value = div.textContent;
+
+    // Focus on the input element to allow the user to enter text
+    input.focus();
+
+    // Add a submit event listener to the input element
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        // Get the text entered by the user
+        const text = input.value;
+
+        // Store the text in local storage
+        localStorage.setItem(div.id, text);
+
+        // Update the div element with the new text
+        div.textContent = text;
+
+        // If the div has no content, show the placeholder text
+        if (div.textContent.trim() === '') {
+          div.textContent = div.getAttribute('data-placeholder');
+        }
+      }
+    });    
   });
+
+  // Check if there is stored text in local storage for the div element
+  const storedText = localStorage.getItem(div.id);
+  if (storedText) {
+    // Update the div element with the stored text
+    div.textContent = storedText;
+  }
 });
